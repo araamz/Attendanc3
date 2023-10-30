a7c21e275afd7d443ccf86fd5f8639234f62dcdb
araamz <araamzaremehrjardi@gmail.com>
2023-10-30 13:10:57 -0700

    Updated Input Element for GroupPage

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,14 @@ import NavigationButton from "./components/NavigationButton.tsx";
 function App(): JSX.Element {
 
   return (
-      <div className="flex flex-col md:flex-row p-5 gap-5">
+      <div className="flex flex-col md:flex-row p-5 gap-5 md:gap-0">
           <nav className="h-min flex place-content-between md:flex-col md:gap-4">
               <NavigationButton icon={'list'} label={'Records'} to='/' />
               <NavigationButton icon={'person'} label={'Students'} to='/students' />
               <NavigationButton icon={'groups'} label={'Groups'} to='/groups' />
               <NavigationButton icon={'settings'} label={'Settings'} to='/settings' />
           </nav>
-          <main className="">
+          <main className="md:ml-auto md:w-4/5 md:overflow-y-auto">
               <Outlet />
           </main>
       </div>
